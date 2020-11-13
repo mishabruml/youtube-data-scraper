aa667db1fe72f33bfb68293e6c3f610d0f8614a7
Misha Bruml <mishabruml@gmail.com>
2020-11-13 12:19:31 +0000

    remove test handler

--- test/handlers/test/index.ts
+++
@@ -1,7 +0,0 @@
-import { handler } from '../../../src/handlers/test'
-
-test('test handler should log hello world', () => {
-  const consoleSpy = jest.spyOn(console, 'log')
-  handler()
-  expect(consoleSpy).toBeCalledWith('hello world')
-})
