37a2b6efcea02dd2e006a3825edf0d9d862b11e8
Misha Bruml <mishabruml@gmail.com>
2020-11-14 17:04:22 +0000

    move handler to scrape playlistIds

--- src/handlers/scrape/youtube/get-upload-playlist-ids.ts
+++
@@ -1,9 +0,0 @@
-import { youtube_v3 } from 'googleapis'
-import { GaxiosResponse } from 'gaxios/build/src/common'
-
-export type UploadPlaylistIds = string[]
-
-export const getUploadPlaylistIds = (listChannelsResponse: GaxiosResponse<youtube_v3.Schema$ChannelListResponse>): UploadPlaylistIds =>
-  listChannelsResponse.data.items?.map(item =>
-    item.contentDetails?.relatedPlaylists?.uploads as string
-  ) ?? []
--- src/handlers/scrape/youtube/list-channels.ts
+++
@@ -1,9 +0,0 @@
-import { youtube_v3 } from 'googleapis'
-import { ChannelIds } from './get-channel-ids'
-
-export const listChannels = (youtubeClient: youtube_v3.Youtube, channelIds: ChannelIds) =>
-  youtubeClient.channels.list({
-    part: ['contentDetails'],
-    maxResults: 1,
-    id: channelIds
-  })
