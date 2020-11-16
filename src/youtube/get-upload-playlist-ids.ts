29450d7e8f6058adc26c94f35f1fc6c1f770ee3e
Misha Bruml <mishabruml@gmail.com>
2020-11-16 23:56:30 +0000

    reduce instead of map

--- src/youtube/get-upload-playlist-ids.ts
+++ src/youtube/get-upload-playlist-ids.ts
@@ -3,6 +3,9 @@ import { GaxiosResponse } from 'gaxios/build/src/common'
 import { PlaylistId, UploadPlaylistIds } from './models'
 
 export const getUploadPlaylistIds = (listChannelsResponse: GaxiosResponse<youtube_v3.Schema$ChannelListResponse>): UploadPlaylistIds =>
-  listChannelsResponse.data.items?.map(item =>
-    item.contentDetails?.relatedPlaylists?.uploads as PlaylistId
-  ) ?? []
+  listChannelsResponse.data.items?.reduce<PlaylistId[]>((filtered, item) => {
+    if (item.contentDetails?.relatedPlaylists?.uploads) {
+      filtered.push(item.contentDetails.relatedPlaylists.uploads)
+    }
+    return filtered
+  }, []) ?? []
